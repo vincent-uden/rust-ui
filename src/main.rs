85d33c6b56e1b49b9777905e66a83a14e2ff9131
Vincent Udén <vincentuden@gmail.com>
2025-08-24 14:22:32 +0200

    Move state to its own file

--- src/main.rs
+++ src/main.rs
@@ -11,58 +11,12 @@ use crate::render::clay::ClayRenderer;
 
 use crate::render::Color;
 use crate::shader::Shader;
+use crate::state::State;
 
 mod geometry;
 mod render;
 mod shader;
-
-pub struct State {
-    pub width: u32,
-    pub height: u32,
-    pub clay: Clay,
-}
-
-impl State {
-    pub fn draw_and_render(&mut self, clay_renderer: &mut ClayRenderer) {
-        let mut clay_scope = self.clay.begin::<(), ()>();
-        clay_scope.with(
-            &Declaration::new()
-                .id(clay_scope.id("red_rectangle"))
-                .layout()
-                .width(Sizing::Percent(1.0))
-                .height(Sizing::Percent(1.0))
-                .padding(Padding::all(24))
-                .end()
-                .corner_radius()
-                .all(5.)
-                .end()
-                .background_color(
-                    Color {
-                        r: 0.0,
-                        g: 0.0,
-                        b: 0.0,
-                        a: 1.0,
-                    }
-                    .into(),
-                )
-                .border()
-                .all_directions(10)
-                .color((0x00, 0xFF, 0x00).into())
-                .end(),
-            |parent| {
-                parent.text(
-                    "Hello, world!",
-                    TextConfig::new()
-                        .font_size(16)
-                        .color((0xff, 0xff, 0xff).into())
-                        .end(),
-                )
-            },
-        );
-        let render_commands: Vec<_> = clay_scope.end().collect();
-        clay_renderer.render_commands(render_commands);
-    }
-}
+mod state;
 
 fn init_open_gl(inital_state: &State) -> (glfw::Glfw, glfw::PWindow) {
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
