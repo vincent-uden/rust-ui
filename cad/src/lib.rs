1a4bbb68f77f51be60d6f4910796b5655ce51bd4
Vincent Udén <vincentuden@gmail.com>
2025-09-13 09:40:42 +0200

    Start work on Scenes

--- cad/src/lib.rs
+++ cad/src/lib.rs
@@ -1,6 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+use crate::sketch::Sketch;
+
 pub mod entity;
 pub mod registry;
 pub mod sketch;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Plane {
+    x: nalgebra::Vector3<f64>,
+    y: nalgebra::Vector3<f64>,
+}
+
+// TODO: Continue defining the scene struct
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Scene {
+    sketches: Vec<Sketch>,
+}
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
